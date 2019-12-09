e6fa608af813b8f75a161a3f0af1a6bbf0429856
mrkshx <57047227+mrkshx@users.noreply.github.com>
2019-12-10 00:32:41 +0100

    added hint to be president if 21 or older

--- checkifIcanvote.js
+++ checkifIcanvote.js
@@ -7,7 +7,10 @@ var yearsTowait = votingAge - myAge;
 // Check if old enogh to vote or not
 
 if ( myAge >= 18 ) {
-	console.log("I am happy to tell you that you are allowed to vote");
+	console.log("I am happy to tell you that you are allowed to vote!");
+	if ( myAge >= 21 ) {
+		console.log("So it seems like you are even old enough to be elected as president!")
+	}
 }
 else {
 	console.log("I am sorry you are not old enough to vote"); 

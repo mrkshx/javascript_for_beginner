ff5a800ba0ebc3a4df520aa00a76fa358a4a70cb
mrkshx <57047227+mrkshx@users.noreply.github.com>
2019-12-10 16:37:14 +0100

    Create basics for task to print first 10 titles of IMDB

---
+++ print10titlesofimdb.js
@@ -0,0 +1,12 @@
+// Task to do: Write a JS script that iterates over the IMDB list of 10 top rated movies and then prints each title.
+
+//Variables
+
+moviepage = 'https://www.imdb.com/chart/top'
+
+
+// Print first 10 titles
+
+for(var i=0; i<=10; i+=1) {
+ console.log{$('moviepage#titleColumn')
+}
